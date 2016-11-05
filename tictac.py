4b67f0283c41769a561d6a4861d62678e3a201a6
Beat <beat.sun@hotmail.com>
2016-11-05 21:10:55 +0100

    Update tictac.py

--- tictac.py
+++ tictac.py
@@ -1,4 +1,4 @@
-
+#Quelle: https://medium.com/technology-invention-and-more/how-to-build-a-simple-neural-network-in-9-lines-of-python-code-cc8f23647ca1#.pea0nt22f
 from numpy import exp, array, random, dot
 
 
