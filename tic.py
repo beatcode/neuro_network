0eac79d86a33754e6023cb4c6721e590474f05f5
beatcode <beat.sun@hotmail.com>
2016-11-10 22:19:47 +0100

    web gui

--- tic.py
+++ tic.py
@@ -11,9 +11,9 @@ class NeuronLayer():
 
 
 class NeuralNetwork():
-    def __init__(self, layer1, layer2):
+    def __init__(self, layer1, layer2, input):
 
-
+        self.input = input
         self.layer1 = layer1
         self.layer2 = layer2
 
@@ -71,113 +71,115 @@ class NeuralNetwork():
 
 if __name__ == "__main__":
 
-    #Seed the random number generator
-random.seed(1)
+        #Seed the random number generator
+    random.seed(1)
 
-    # Create layer 1 (4 neurons, each with 3 inputs)
-layer1 = NeuronLayer(10, 9)
+        # Create layer 1 (4 neurons, each with 3 inputs)
+    layer1 = NeuronLayer(10, 9)
 
-    # Create layer 2 (a single neuron with 4 inputs)
-layer2 = NeuronLayer(9, 10)
+        # Create layer 2 (a single neuron with 4 inputs)
+    layer2 = NeuronLayer(9, 10)
 
-    # Combine the layers to create a neural network
-neural_network = NeuralNetwork(layer1, layer2)
+    # Parameter vom Web
+    input = [ sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9] ]
 
-print "Stage 1) Random starting synaptic weights: </br>"
-neural_network.print_weights()
+        # Combine the layers to create a neural network
+    neural_network = NeuralNetwork(layer1, layer2, input)
 
-
-    # The training set. We have 7 examples, each consisting of 3 input values
-    # and 1 output value.
-
-training_set_inputs = array([     
-[ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ],
-[ 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ],
-[ 1, 0.5, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 0.5 ],
-[ 1, 0.5, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 1 ],
-[ 1, 0.5, 0.5, 0.5, 0, 0.5, 0.5, 0, 1 ],
-[ 1, 1, 0.5, 0.5, 0, 0.5, 0.5, 0, 1 ],
-[ 1, 1, 0, 0.5, 0, 0.5, 0.5, 0, 1 ],
-[ 1, 1, 0, 0.5, 0, 0.5, 1, 0, 1 ],
-[ 1, 1, 0, 0, 0, 0.5, 1, 0, 1 ],
-
-[ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ],
-[ 0.5, 0.5, 0.5, 0.5, 1, 0.5, 0.5, 0.5, 0.5 ],
-[ 0.5, 0.5, 0, 0.5, 1, 0.5, 0.5, 0.5, 0.5 ],
-[ 0.5, 0.5, 0, 0.5, 1, 0.5, 0.5, 1, 0.5 ],
-[ 0.5, 0, 0, 0.5, 1, 0.5, 0.5, 1, 0.5 ],
-[ 1, 0, 0, 0.5, 1, 0.5, 0.5, 1, 0.5 ], 
-[ 1, 0, 0, 0.5, 1, 0, 0.5, 1, 0.5 ],
-[ 1, 0, 0, 0.5, 1, 0, 0.5, 1, 1 ],
-[ 1, 0, 0, 0.5, 1, 0, 0, 1, 1 ],
-
-[ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ], 
-[ 0.5, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ],
-[ 0.5, 1, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 0.5 ],
-[ 0.5, 1, 0.5, 0.5, 0, 0.5, 0.5, 1, 0.5 ],
-[ 0.5, 1, 0, 0.5, 0, 0.5, 0.5, 1, 0.5 ],
-[ 0.5, 1, 0, 0.5, 0, 0.5, 1, 1, 0.5 ],
-[ 0.5, 1, 0, 0, 0, 0.5, 1, 1, 0.5 ],
-[ 0.5, 1, 0, 0, 0, 1, 1, 1, 0.5 ],
-[ 0.5, 1, 0, 0, 0, 1, 1, 1, 0 ]])
-  
+    print "Stage 1) Random starting synaptic weights: </br>"
+    neural_network.print_weights()
 
 
-training_set_outputs = array([      
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 			0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 1, 		0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1],
-[0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 1, 		0.5, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 		1, 1, 1, 1, 1, 1, 1, 1, 1],
-[0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 0, 0, 		0.5, 0, 0, 0, 0, 0, 0, 0, 0, 			0.5, 0.5, 0.5, 0, 0, 0, 0, 0, 0],
-[0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 	0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 	0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 0, 0], 
-[0.5, 0, 0, 0, 0, 0, 0, 0, 0,	 		1, 1, 1, 1, 1, 1, 1, 1, 1, 			0.5, 0, 0, 0, 0, 0, 0, 0, 0],
-[0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 	0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 0, 0, 		0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 1, 1],
-[0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 1, 1, 	0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 	0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 1],
-[0.5, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 		0.5, 0.5, 1, 1, 1, 1, 1, 1, 1, 			0.5, 0.5, 1, 1, 1, 1, 1, 1, 1],
-[0.5, 0.5, 1, 1, 1, 1, 1, 1, 1, 		0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 1, 1, 		0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0, 0]]).T
+        # The training set. We have 7 examples, each consisting of 3 input values
+        # and 1 output value.
+
+    training_set_inputs = array([     
+    [ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ],
+    [ 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ],
+    [ 1, 0.5, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 0.5 ],
+    [ 1, 0.5, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 1 ],
+    [ 1, 0.5, 0.5, 0.5, 0, 0.5, 0.5, 0, 1 ],
+    [ 1, 1, 0.5, 0.5, 0, 0.5, 0.5, 0, 1 ],
+    [ 1, 1, 0, 0.5, 0, 0.5, 0.5, 0, 1 ],
+    [ 1, 1, 0, 0.5, 0, 0.5, 1, 0, 1 ],
+    [ 1, 1, 0, 0, 0, 0.5, 1, 0, 1 ],
+
+    [ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ],
+    [ 0.5, 0.5, 0.5, 0.5, 1, 0.5, 0.5, 0.5, 0.5 ],
+    [ 0.5, 0.5, 0, 0.5, 1, 0.5, 0.5, 0.5, 0.5 ],
+    [ 0.5, 0.5, 0, 0.5, 1, 0.5, 0.5, 1, 0.5 ],
+    [ 0.5, 0, 0, 0.5, 1, 0.5, 0.5, 1, 0.5 ],
+    [ 1, 0, 0, 0.5, 1, 0.5, 0.5, 1, 0.5 ], 
+    [ 1, 0, 0, 0.5, 1, 0, 0.5, 1, 0.5 ],
+    [ 1, 0, 0, 0.5, 1, 0, 0.5, 1, 1 ],
+    [ 1, 0, 0, 0.5, 1, 0, 0, 1, 1 ],
+
+    [ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ], 
+    [ 0.5, 1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 ],
+    [ 0.5, 1, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 0.5 ],
+    [ 0.5, 1, 0.5, 0.5, 0, 0.5, 0.5, 1, 0.5 ],
+    [ 0.5, 1, 0, 0.5, 0, 0.5, 0.5, 1, 0.5 ],
+    [ 0.5, 1, 0, 0.5, 0, 0.5, 1, 1, 0.5 ],
+    [ 0.5, 1, 0, 0, 0, 0.5, 1, 1, 0.5 ],
+    [ 0.5, 1, 0, 0, 0, 1, 1, 1, 0.5 ],
+    [ 0.5, 1, 0, 0, 0, 1, 1, 1, 0 ]])
+      
+
+
+    training_set_outputs = array([      
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 			     0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 1, 		    0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1],
+    [0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 1, 		 0.5, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 		        1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 0, 0, 		 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 			        0.5, 0.5, 0.5, 0, 0, 0, 0, 0, 0],
+    [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 	 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 	0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 0, 0], 
+    [0.5, 0, 0, 0, 0, 0, 0, 0, 0,	 		     1, 1, 1, 1, 1, 1, 1, 1, 1, 			        0.5, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1,  0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 0, 0, 		    0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 1, 1],
+    [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 1, 1, 	 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0, 0, 	    0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 1],
+    [0.5, 0.5, 0.5, 0, 0, 0, 0, 0, 0, 		     0.5, 0.5, 1, 1, 1, 1, 1, 1, 1, 			    0.5, 0.5, 1, 1, 1, 1, 1, 1, 1],
+    [0.5, 0.5, 1, 1, 1, 1, 1, 1, 1, 		     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1, 1, 1, 		0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0, 0]]).T
 
 
 
 
-    # Train the neural network using the training set.
-    # Do it 60,000 times and make small adjustments each time.
-neural_network.train(training_set_inputs, training_set_outputs, 100)
-print "Stage 2) New synaptic weights after training: </br>"
+        # Train the neural network using the training set.
+        # Do it 60,000 times and make small adjustments each time.
+    neural_network.train(training_set_inputs, training_set_outputs, 100)
+    print "Stage 2) New synaptic weights after training: </br>"
 
 
-    # Test the neural network with a new situation.
-print "Berechnung Runde 2 [1, -, -, -, -, -, -, -, -] -> ?: </br>"
-hidden_state, output = neural_network.think(array([1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
-print output
+        # Test the neural network with a new situation.
+    print "Berechnung Runde 2 [1, -, -, -, -, -, -, -, -] -> ?: </br>"
+    hidden_state, output = neural_network.think(array([1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
+    print output
 
-print " Berechnung Runde 3 </br> "
-hidden_state, output = neural_network.think(output)
-print output
+    print " Berechnung Runde 3 </br> "
+    hidden_state, output = neural_network.think(output)
+    print output
 
-print "Berechnung Runde 4 </br>"
-hidden_state, output = neural_network.think(output)
-print output
+    print "Berechnung Runde 4 </br>"
+    hidden_state, output = neural_network.think(output)
+    print output
 
-print "Berechnung Runde 5 </br>"
-hidden_state, output = neural_network.think(output)
-print output
+    print "Berechnung Runde 5 </br>"
+    hidden_state, output = neural_network.think(output)
+    print output
 
-print "Berechnung Runde 6 </br>"
-hidden_state, output = neural_network.think(output)
-print output
+    print "Berechnung Runde 6 </br>"
+    hidden_state, output = neural_network.think(output)
+    print output
 
-print "Berechnung Runde 7 </br>"
-hidden_state, output = neural_network.think(output)
-print output
+    print "Berechnung Runde 7 </br>"
+    hidden_state, output = neural_network.think(output)
+    print output
 
-print "Berechnung Runde 8 </br>"
-hidden_state, output = neural_network.think(output)
-print output
+    print "Berechnung Runde 8 </br>"
+    hidden_state, output = neural_network.think(output)
+    print output
 
-print "Berechnung Runde 9 </br>"
-hidden_state, output = neural_network.think(output)
-print output
+    print "Berechnung Runde 9 </br>"
+    hidden_state, output = neural_network.think(output)
+    print output
 
-   
-test = array([ sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9] ])
-test2 = sys.argv[1]
-print test2
+    print neural_network.input
+    # 
+     
 
--- web.php
+++ web.php
@@ -1,4 +1,33 @@
 
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+ 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<script>
+
+
+function setTableCellVal(input) {
+document.getElementById(input).value='x' 
+}
+
+
+
+</script>
+
+
+
+<div class="page-header">
+  <h1>Neuronales Netzwerk <small>Tic Tac Toe</small></h1>
+</div>
+
 <?php
 if($_POST['submit'])
 {
@@ -6,63 +35,152 @@ if($_POST['submit'])
 $daten = $_POST['tictactoe'];
 
 print_r($daten);
+        // Default Value setzen
+        if ($daten[1] == '') {
+            $daten[1] = 0.5;
+             } elseif ($daten[1] == 'x') {
+                $daten[1] = 1;
+            }  elseif ($daten[1] == 'o') {
+                $daten[1] = 0;
+            } else {
+                $daten[1] = 0.5;
+            }
+        if ($daten[2]== '') {
+            $daten[2] = 0.5;
+            } elseif ($daten[2] == 'x') {
+                $daten[2] = 1;
+            }  elseif ($daten[2] == 'o') {
+                $daten[2] = 0;
+            } else {
+                $daten[2] = 0.5;
+            }
 
-        //  $command = "python /var/www/html/test/tic.py";
-        $command = "python /var/www/html/test/tic.py $daten[1] $daten[2] $daten[3] $daten[4] $daten[5] $daten[6] $daten[7] $daten[8] $daten[9]";
+        if ($daten[3]== '') {
+            $daten[3] = 0.5;
+            } elseif ($daten[3] == 'x') {
+                $daten[3] = 1;
+            }  elseif ($daten[3] == 'o') {
+                $daten[3] = 0;
+            } else {
+                $daten[3] = 0.5;
+            }
+
+        if ($daten[4]== '') {
+            $daten[4] = 0.5;
+            } elseif ($daten[4] == 'x') {
+                $daten[4] = 1;
+            }  elseif ($daten[4] == 'o') {
+                $daten[4] = 0;
+            } else {
+                $daten[4] = 0.5;
+            }
+
+        if ($daten[5]== '') {
+            $daten[5] = 0.5;
+            } elseif ($daten[5] == 'x') {
+                $daten[5] = 1;
+            }  elseif ($daten[5] == 'o') {
+                $daten[5] = 0;
+            } else {
+                $daten[5] = 0.5;
+            }
+
+        if ($daten[6]== '') {
+            $daten[6] = 0.5;
+            } elseif ($daten[6] == 'x') {
+                $daten[6] = 1;
+            }  elseif ($daten[6] == 'o') {
+                $daten[6] = 0;
+            } else {
+                $daten[6] = 0.5;
+            }
+
+        if ($daten[7]== '') {
+            $daten[7] = 0.5;
+            } elseif ($daten[7] == 'x') {
+                $daten[7] = 1;
+            }  elseif ($daten[7] == 'o') {
+                $daten[7] = 0;
+            } else {
+                $daten[7] = 0.5;
+            }
+
+        if ($daten[8]== '') {
+            $daten[8] = 0.5;
+            } elseif ($daten[8] == 'x') {
+                $daten[8] = 1;
+            }  elseif ($daten[8] == 'o') {
+                $daten[8] = 0;
+            } else {
+                $daten[8] = 0.5;
+            }
+
+        if ($daten[9]== '') {
+            $daten[9] = 0.5;
+            } elseif ($daten[9] == 'x') {
+                $daten[9] = 1;
+            }  elseif ($daten[9] == 'o') {
+                $daten[9] = 0;
+            } else {
+                $daten[9] = 0.5;
+            }
+  
+        $command = "python /var/www/html/tic.py $daten[1] $daten[2] $daten[3] $daten[4] $daten[5] $daten[6] $daten[7] $daten[8] $daten[9] ";
 
         $temp = passthru($command);
+     
         print $temp;
 
 } else {
 
 ?>
 <form action="" id="form" name="form" method="post">
-<table>
+<table id="table1">
     <tr>
-        <td>
-            <input type="text" name="tictactoe[1]" input="1"  maxlength="1" size="1"></td>
+        <td onclick="setTableCellVal('1')">
+            <input id="1"  type="text" name="tictactoe[1]" input="1"  maxlength="1" size="1"></td>
         </td>
-        <td>
-            <input type="text" name="tictactoe[2]"  maxlength="1" size="1"></td>
+        <td onclick="setTableCellVal('2')">
+            <input id="2" type="text" name="tictactoe[2]"  maxlength="1" size="1" > </td>
         </td>
-        <td>
-            <input type="text" name="tictactoe[3]"  maxlength="1" size="1"></td>
+        <td onclick="setTableCellVal('3')">
+            <input id="3" type="text" name="tictactoe[3]"  maxlength="1" size="1"></td>
         </td>
    </tr>
    <tr>
-        <td>
-            <input type="text" name="tictactoe[4]"  maxlength="1" size="1"></td>
+       <td onclick="setTableCellVal('4')">
+            <input id="4" type="text" name="tictactoe[4]"  maxlength="1" size="1"></td>
         </td>
-        <td>
-            <input type="text" name="tictactoe[5]"  maxlength="1" size="1"></td>
+       <td onclick="setTableCellVal('5')">
+            <input id="5" type="text" name="tictactoe[5]"  maxlength="1" size="1"></td>
         </td>
-        <td>
-            <input type="text" name="tictactoe[6]"  maxlength="1" size="1"></td>
+        <td onclick="setTableCellVal('6')">
+            <input id="6" type="text" name="tictactoe[6]"  maxlength="1" size="1"></td>
         </td>
     </tr>
     <tr>
-        <td>
-            <input type="text" name="tictactoe[7]"  maxlength="1" size="1"></td>
+        <td onclick="setTableCellVal('7')">
+            <input id="7" type="text" name="tictactoe[7]"  maxlength="1" size="1"></td>
         </td>
-        <td>
-            <input type="text" name="tictactoe[8]"  maxlength="1" size="1"></td>
+       <td onclick="setTableCellVal('8')">
+            <input id="8" type="text" name="tictactoe[8]"  maxlength="1" size="1"></td>
         </td>
-        <td>
-            <input type="text" name="tictactoe[9]"  maxlength="1" size="1"></td>
+       <td onclick="setTableCellVal('9')">
+            <input id="9" type="text" name="tictactoe[9]"  maxlength="1" size="1"></td>
             <input type="hidden" name="submit"  value="x" input="1"  maxlength="1" size="1"></td>
 
         </td>
      </tr>
-            <button type="submit" name="SubmitButton"  >an Netzwerk senden</button>
+            
     
 </tr>
 
 </table>
-
+<br>
+<button type="submit" name="SubmitButton"  >an Netzwerk senden</button>
 </form>
 <?php
 }
 ?>
-
-
+</div>
 
