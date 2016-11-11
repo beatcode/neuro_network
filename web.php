8f1b3d93c7c0b2b16b190114f1e26dac84d6a8cf
beatcode <beat.sun@hotmail.com>
2016-11-11 01:56:39 -0500

    webgui

--- web.php
+++ web.php
@@ -1,27 +1,8 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
- 
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-<script>
-
-function setval(input) {
-document.getElementById(input).value='x' 
-}
-
-</script>
-
-<div class="page-header">
-  <h1>Neuronales Netzwerk <small>Tic Tac Toe</small></h1>
-</div>
 
 <?php
 if($_POST['submit'])
@@ -120,60 +101,59 @@ print_r($daten);
                 $daten[9] = 0.5;
             }
   
-        $command = "python /var/www/html/tic.py $daten[1] $daten[2] $daten[3] $daten[4] $daten[5] $daten[6] $daten[7] $daten[8] $daten[9] ";
-
+        $command = "python /var/www/html/neuronal_network/tic.py $daten[1] $daten[2] $daten[3] $daten[4] $daten[5] $daten[6] $daten[7] $daten[8] $daten[9] ";
         $temp = passthru($command);
-     
         print $temp;
 
 } else {
 
 ?>
+
 <form action="" id="form" name="form" method="post">
-<table id="table1">
-    <tr>
-        <td onclick="setval('1')">
-            <input id="1"  type="text" name="tictactoe[1]" input="1"  maxlength="1" size="1"></td>
-        </td>
-        <td onclick="setval('2')">
-            <input id="2" type="text" name="tictactoe[2]"  maxlength="1" size="1" > </td>
-        </td>
-        <td onclick="setval('3')">
-            <input id="3" type="text" name="tictactoe[3]"  maxlength="1" size="1"></td>
-        </td>
-   </tr>
-   <tr>
-       <td onclick="setval('4')">
-            <input id="4" type="text" name="tictactoe[4]"  maxlength="1" size="1"></td>
-        </td>
-       <td onclick="setval('5')">
-            <input id="5" type="text" name="tictactoe[5]"  maxlength="1" size="1"></td>
-        </td>
-        <td onclick="setval('6')">
-            <input id="6" type="text" name="tictactoe[6]"  maxlength="1" size="1"></td>
-        </td>
-    </tr>
-    <tr>
-        <td onclick="setval('7')">
-            <input id="7" type="text" name="tictactoe[7]"  maxlength="1" size="1"></td>
-        </td>
-       <td onclick="setval('8')">
-            <input id="8" type="text" name="tictactoe[8]"  maxlength="1" size="1"></td>
-        </td>
-       <td onclick="setval('9')">
-            <input id="9" type="text" name="tictactoe[9]"  maxlength="1" size="1"></td>
-            <input type="hidden" name="submit"  value="x" input="1"  maxlength="1" size="1"></td>
 
-        </td>
-     </tr>
-            
-    
-</tr>
 
+<title> Tic-Tac-Toe </title>    
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript" charset="utf-8"></script>  
+<link type="text/css" rel="stylesheet" href="style.css" />  
+
+<body>
+
+<script>
+
+function setval(input) {
+document.getElementById(input).value='x' 
+}
+
+</script>
+
+
+<table id ='board'>
+  <tr>
+    <td  onclick="setval('1')"> <input id="1" type="text" name="lname">  </td>
+ <td  onclick="setval('2')"> <input id="2" type="text" name="lname">  </td>
+ <td  onclick="setval('3')"> <input id="3" type="text" name="lname">  </td>
+  </tr>
+  <tr>
+    <td></td>
+   <td></td>
+   <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td> 
+    <td></td>
+  </tr>
 </table>
+
+
+<h1><i class="icon-circle-blank"></i></h1>
+
+<h1><i class="icon-circle-romve"></i></h1>
 <br>
 <button type="submit" name="SubmitButton"  >an Netzwerk senden</button>
 </form>
+
+
 <?php
 }
 ?>
