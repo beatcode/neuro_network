28e45e9637d3eaff6be83cf16dd66c822d3b8e89
beatcode <beat.sun@hotmail.com>
2016-11-17 17:58:36 -0500

    javascript

--- web.php
+++ web.php
@@ -20,100 +20,20 @@ session_start();
 
 if (!empty($_POST)){
 
+// TOdo übergabe der Inputparamter
 
-$daten = $_POST['tictactoe'];
+$daten[1]= '1';
+$daten[2]= '1';
+$daten[3]= '1';
+$daten[4]= '1';
+$daten[5]= '1';
+$daten[6]= '1';
+$daten[7]= '1';
+$daten[8]= '1';
+$daten[9]= '1';
 
-print_r($daten);
-        // Default Value setzen
-        if ($daten[1] == '') {
-            $daten[1] = 0.5;
-             } elseif ($daten[1] == 'x') {
-                $daten[1] = 1;
-            }  elseif ($daten[1] == 'o') {
-                $daten[1] = 0;
-            } else {
-                $daten[1] = 0.5;
-            }
-        if ($daten[2]== '') {
-            $daten[2] = 0.5;
-            } elseif ($daten[2] == 'x') {
-                $daten[2] = 1;
-            }  elseif ($daten[2] == 'o') {
-                $daten[2] = 0;
-            } else {
-                $daten[2] = 0.5;
-            }
 
-        if ($daten[3]== '') {
-            $daten[3] = 0.5;
-            } elseif ($daten[3] == 'x') {
-                $daten[3] = 1;
-            }  elseif ($daten[3] == 'o') {
-                $daten[3] = 0;
-            } else {
-                $daten[3] = 0.5;
-            }
-
-        if ($daten[4]== '') {
-            $daten[4] = 0.5;
-            } elseif ($daten[4] == 'x') {
-                $daten[4] = 1;
-            }  elseif ($daten[4] == 'o') {
-                $daten[4] = 0;
-            } else {
-                $daten[4] = 0.5;
-            }
-
-        if ($daten[5]== '') {
-            $daten[5] = 0.5;
-            } elseif ($daten[5] == 'x') {
-                $daten[5] = 1;
-            }  elseif ($daten[5] == 'o') {
-                $daten[5] = 0;
-            } else {
-                $daten[5] = 0.5;
-            }
-
-        if ($daten[6]== '') {
-            $daten[6] = 0.5;
-            } elseif ($daten[6] == 'x') {
-                $daten[6] = 1;
-            }  elseif ($daten[6] == 'o') {
-                $daten[6] = 0;
-            } else {
-                $daten[6] = 0.5;
-            }
-
-        if ($daten[7]== '') {
-            $daten[7] = 0.5;
-            } elseif ($daten[7] == 'x') {
-                $daten[7] = 1;
-            }  elseif ($daten[7] == 'o') {
-                $daten[7] = 0;
-            } else {
-                $daten[7] = 0.5;
-            }
-
-        if ($daten[8]== '') {
-            $daten[8] = 0.5;
-            } elseif ($daten[8] == 'x') {
-                $daten[8] = 1;
-            }  elseif ($daten[8] == 'o') {
-                $daten[8] = 0;
-            } else {
-                $daten[8] = 0.5;
-            }
-
-        if ($daten[9]== '') {
-            $daten[9] = 0.5;
-            } elseif ($daten[9] == 'x') {
-                $daten[9] = 1;
-            }  elseif ($daten[9] == 'o') {
-                $daten[9] = 0;
-            } else {
-                $daten[9] = 0.5;
-            }
-  
+// Aufruf des Python script mit den Inputparameter  
         $command = "python /var/www/html/neuronal_network/tic.py $daten[1] $daten[2] $daten[3] $daten[4] $daten[5] $daten[6] $daten[7] $daten[8] $daten[9] ";
         $temp = passthru($command);
         print $temp;
