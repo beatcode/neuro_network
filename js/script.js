e46018b366736c0f9715b81890daaa24a24fe4d0
beatcode <beat.sun@hotmail.com>
2016-11-21 15:53:41 -0500

    structur

---
+++ js/script.js
@@ -0,0 +1,153 @@
+
+function python() {
+
+    if ( $(".chk").is(":checked")) {
+
+    } else {
+
+        $("#output").val(ReadInput());
+        $.ajax({
+            type: 'POST',
+            url: "/neuronal_network/PythonCall_play.php",
+            data: "input="+ ReadInput(),
+            success: function(data){
+                $("#calc").val(data);
+                $("#input").val(data);
+                set_output(data);
+            }
+        });
+
+        count();
+
+    }}
+
+$(document).ready(function(){
+
+    $(".chk").click(function() {
+        clear_content();
+    });
+});
+
+
+function set_firstplayer() {
+
+    player = $("#zug").val();
+
+    if  (player == "0") {
+        $("#player").val("1");
+    } else if (player == "1" ) {
+        $("#player").val("2");
+    }
+
+}
+
+function ReadInput() {
+
+    feld1 = document.getElementById("1").value;
+    feld2 = document.getElementById("2").value;
+    feld3 = document.getElementById("3").value;
+    feld4 = document.getElementById("4").value;
+    feld5 = document.getElementById("5").value;
+    feld6 = document.getElementById("6").value;
+    feld7 = document.getElementById("7").value;
+    feld8 = document.getElementById("8").value;
+    feld9 = document.getElementById("9").value;
+
+    var result = [feld1,feld2,feld3,feld4,feld5,feld6,feld7,feld8,feld9];
+
+
+    return result;
+
+}
+
+
+function clear_content() {
+
+    $("#input").val("0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5");
+    $("#output").val("");
+    $("#calc").val("");
+    $("#zug").val("0");
+    $("#player").val("-");
+
+    var input = "0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5";
+    set_output(input);
+
+}
+
+
+function set_output(val) {
+
+    var output = val;
+    output = output.replace(/\s/g, "") ;
+    var splits = output.split(",");
+
+    setval_compute('0', '0', '1',  splits[0]);
+    setval_compute('0', '1', '2',  splits[1]);
+    setval_compute('0', '2', '3',  splits[2]);
+    setval_compute('1', '0', '4',  splits[3]);
+    setval_compute('1', '1', '5',  splits[4]);
+    setval_compute('1', '2', '6',  splits[5]);
+    setval_compute('2', '0', '7',  splits[6]);
+    setval_compute('2', '1', '8',  splits[7]);
+    setval_compute('2', '2', '9',  splits[8]);
+}
+
+function count_Zug() {
+
+    counter = $("#zug").val();
+    counter++;
+    $("#zug").val(counter);
+}
+
+function get_player() {
+    return $("#gezogen").val();
+}
+
+
+function set_player(val) {
+    $("#gezogen").val(val);
+
+}
+
+function setval_human(row,cell,feld, wert) {
+
+
+
+    act_field = document.getElementById(feld).value;
+
+
+    if ( $(".chk").is(":checked")) {
+// training
+
+
+    } else   {
+
+        set_firstplayer();
+
+// Play
+        if( act_field != '0.5' ) {
+
+        } else if ( wert == '0.0') {
+            document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-circle-thin"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+        } else if (wert == '1.0') {
+            document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-times"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+        } else {
+            document.getElementById("board").rows[row].cells[cell].innerHTML='<input type = "hidden" id="' + feld + '"  value="0.5" >';
+        }
+
+        $("#output").val(ReadInput());
+        count_Zug();
+        set_player("1");
+    }
+
+}
+
+function setval_compute(row,cell,feld, wert) {
+    if ( wert == 0.0) {
+        document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-circle-thin"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+    } else if (wert == 1.0) {
+        document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-times"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+    } else {
+        document.getElementById("board").rows[row].cells[cell].innerHTML='<input type = "hidden" id="' + feld + '"  value="0.5" >';
+    }
+}
