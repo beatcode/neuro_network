5f3eda2ba7858e6e903974ab2c4ba9dbf971db8c
beatcode <beat.sun@hotmail.com>
2016-11-22 06:54:11 -0500

    indent

--- js/script.js
+++ js/script.js
@@ -1,171 +1,168 @@
-
-$(document).ready(function(){
-    $(".chk").click(function() {
-        alert('In Entwicklung');
-        clear_content();
-    });
+$(document).ready(function() {
+  $(".chk").click(function() {
+    alert('In Entwicklung');
+    clear_content();
+  });
 });
-
 
 
 function python() {
 
-    if ( $(".chk").is(":checked")) {
+  if ($(".chk").is(":checked")) {
 
-    } else {
-        set_player();
-        $("#output").val(ReadInput());
-        $.ajax({
-            type: 'POST',
-            url: "/neuronal_network/ajax/PythonCall_play.php",
-            data: "input="+ ReadInput(),
-            success: function(data){
-                $("#calc").val(data);
-                $("#input").val(data);
-                set_output(data);
-            }
-        });
+  } else {
+    set_player();
+    $("#output").val(ReadInput());
+    $.ajax({
+      type: 'POST',
+      url: "/neuronal_network/ajax/PythonCall_play.php",
+      data: "input=" + ReadInput(),
+      success: function(data) {
+        $("#calc").val(data);
+        $("#input").val(data);
+        set_output(data);
+      }
+    });
 
-        count();
-
-    }}
+    count();
+  }
+}
 
 
 
 function set_player() {
 
-    player = $("#player").val();
+  player = $("#player").val();
 
-    if  (player == "1") {
-        $("#player").val("2");
-    } else if (player == "2") {
-        $("#player").val("1");
-    }
+  if (player == "1") {
+    $("#player").val("2");
+  } else if (player == "2") {
+    $("#player").val("1");
+  }
 }
 
 function ReadInput() {
 
-    feld1 = document.getElementById("1").value;
-    feld2 = document.getElementById("2").value;
-    feld3 = document.getElementById("3").value;
-    feld4 = document.getElementById("4").value;
-    feld5 = document.getElementById("5").value;
-    feld6 = document.getElementById("6").value;
-    feld7 = document.getElementById("7").value;
-    feld8 = document.getElementById("8").value;
-    feld9 = document.getElementById("9").value;
+  feld1 = document.getElementById("1").value;
+  feld2 = document.getElementById("2").value;
+  feld3 = document.getElementById("3").value;
+  feld4 = document.getElementById("4").value;
+  feld5 = document.getElementById("5").value;
+  feld6 = document.getElementById("6").value;
+  feld7 = document.getElementById("7").value;
+  feld8 = document.getElementById("8").value;
+  feld9 = document.getElementById("9").value;
 
-    var result = [feld1,feld2,feld3,feld4,feld5,feld6,feld7,feld8,feld9];
-    return result;
+  var result = [feld1, feld2, feld3, feld4, feld5, feld6, feld7, feld8, feld9];
+  return result;
 }
 
 
 function clear_content() {
 
-    $("#input").val("0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5");
-    $("#output").val("");
-    $("#calc").val("");
-    $("#zug").val("0");
-    $("#player").val("1");
+  $("#input").val("0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5");
+  $("#output").val("");
+  $("#calc").val("");
+  $("#zug").val("0");
+  $("#player").val("1");
 
-    var input = "0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5";
-    set_output(input);
+  var input = "0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5";
+  set_output(input);
 
 }
 
 
 function set_output(val) {
 
-    var output = val;
-    output = output.replace(/\s/g, "") ;
-    var splits = output.split(",");
+  var output = val;
+  output = output.replace(/\s/g, "");
+  var splits = output.split(",");
 
-    setval_compute('0', '0', '1',  splits[0]);
-    setval_compute('0', '1', '2',  splits[1]);
-    setval_compute('0', '2', '3',  splits[2]);
-    setval_compute('1', '0', '4',  splits[3]);
-    setval_compute('1', '1', '5',  splits[4]);
-    setval_compute('1', '2', '6',  splits[5]);
-    setval_compute('2', '0', '7',  splits[6]);
-    setval_compute('2', '1', '8',  splits[7]);
-    setval_compute('2', '2', '9',  splits[8]);
+  setval_compute('0', '0', '1', splits[0]);
+  setval_compute('0', '1', '2', splits[1]);
+  setval_compute('0', '2', '3', splits[2]);
+  setval_compute('1', '0', '4', splits[3]);
+  setval_compute('1', '1', '5', splits[4]);
+  setval_compute('1', '2', '6', splits[5]);
+  setval_compute('2', '0', '7', splits[6]);
+  setval_compute('2', '1', '8', splits[7]);
+  setval_compute('2', '2', '9', splits[8]);
 }
 
 function count_Zug() {
 
-    counter = $("#zug").val();
-    counter++;
-    $("#zug").val(counter);
+  counter = $("#zug").val();
+  counter++;
+  $("#zug").val(counter);
 }
 
 function get_value() {
-    var player = $("#player").val();
-    if (player == "1") {
-        return "1.0";
-    } else if (player == "2") {
-        return "0.0";
-    }
+  var player = $("#player").val();
+  if (player == "1") {
+    return "1.0";
+  } else if (player == "2") {
+    return "0.0";
+  }
 
 }
 
 
 
-function setval_human(row,cell,feld, wert) {
+function setval_human(row, cell, feld, wert) {
 
-    var wert = get_value();
-
-
-    act_field = document.getElementById(feld).value;
-
-    if ( $(".chk").is(":checked")) {
-        // training
-
-        // Play
-        if( act_field != '0.5' ) {
-
-        } else if ( wert == '0.0') {
-            document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-circle-thin"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
-        } else if (wert == '1.0') {
-            document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-times"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
-        } else {
-            document.getElementById("board").rows[row].cells[cell].innerHTML='<input type = "hidden" id="' + feld + '"  value="0.5" >';
-        }
-
-        $("#output").val(ReadInput());
-        count_Zug();
-        set_player();
+  var wert = get_value();
 
 
-    } else   {
+  act_field = document.getElementById(feld).value;
 
+  if ($(".chk").is(":checked")) {
+    // training
 
+    // Play
+    if (act_field != '0.5') {
 
-        // Play
-        if( act_field != '0.5' ) {
-
-        } else if ( wert == '0.0') {
-            document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-circle-thin"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
-        } else if (wert == '1.0') {
-            document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-times"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
-        } else {
-            document.getElementById("board").rows[row].cells[cell].innerHTML='<input type = "hidden" id="' + feld + '"  value="0.5" >';
-        }
-
-        $("#output").val(ReadInput());
-        count_Zug();
-        set_player();
-    }
-
-}
-
-function setval_compute(row,cell,feld, wert) {
-    if ( wert == 0.0) {
-        document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-circle-thin"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
-    } else if (wert == 1.0) {
-        document.getElementById("board").rows[row].cells[cell].innerHTML='<h2><i class="fa fa-times"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+    } else if (wert == '0.0') {
+      document.getElementById("board").rows[row].cells[cell].innerHTML = '<h2><i class="fa fa-circle-thin"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+    } else if (wert == '1.0') {
+      document.getElementById("board").rows[row].cells[cell].innerHTML = '<h2><i class="fa fa-times"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
     } else {
-        document.getElementById("board").rows[row].cells[cell].innerHTML='<input type = "hidden" id="' + feld + '"  value="0.5" >';
+      document.getElementById("board").rows[row].cells[cell].innerHTML = '<input type = "hidden" id="' + feld + '"  value="0.5" >';
     }
+
+    $("#output").val(ReadInput());
+    count_Zug();
+    set_player();
+
+
+  } else {
+
+
+
+    // Play
+    if (act_field != '0.5') {
+
+    } else if (wert == '0.0') {
+      document.getElementById("board").rows[row].cells[cell].innerHTML = '<h2><i class="fa fa-circle-thin"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+    } else if (wert == '1.0') {
+      document.getElementById("board").rows[row].cells[cell].innerHTML = '<h2><i class="fa fa-times"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+    } else {
+      document.getElementById("board").rows[row].cells[cell].innerHTML = '<input type = "hidden" id="' + feld + '"  value="0.5" >';
+    }
+
+    $("#output").val(ReadInput());
+    count_Zug();
+    set_player();
+  }
 
 }
 
+function setval_compute(row, cell, feld, wert) {
+  if (wert == 0.0) {
+    document.getElementById("board").rows[row].cells[cell].innerHTML = '<h2><i class="fa fa-circle-thin"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+  } else if (wert == 1.0) {
+    document.getElementById("board").rows[row].cells[cell].innerHTML = '<h2><i class="fa fa-times"></i></h2> <input type = "hidden" id="' + feld + '"  value="' + wert + '" >';
+  } else {
+    document.getElementById("board").rows[row].cells[cell].innerHTML = '<input type = "hidden" id="' + feld + '"  value="0.5" >';
+  }
+
+}
--- web.php
+++ web.php
@@ -1,103 +1,103 @@
 <!DOCTYPE html>
 <html lang="de">
+
 <head>
 
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript" charset="utf-8"></script>
-    <script language="javascript" type="text/javascript" src="js/script.js"></script>
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link type="text/css" rel="stylesheet" href="css/style.css" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+  <script language="javascript" type="text/javascript" src="js/script.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link type="text/css" rel="stylesheet" href="css/style.css" />
 
-    <title> Tic-Tac-Toe </title>
+  <title> Tic-Tac-Toe </title>
 
-    <div class="page-header">
-        <h1>Neuronales Netzwerk</h1>
-    </div>
+  <div class="page-header">
+    <h1>Neuronales Netzwerk</h1>
+  </div>
 
-<body>
+  <body>
 
-<div class="container">
-    <div class="row">
+    <div class="container">
+      <div class="row">
         <div class="col-sm-6">
-            <table id ='board'>
-                <tr>
-                    <td before="test()"  onclick="setval_human('0', '0', '1', '1.0')"> <input type="hidden" id="1" value="0.5"> </td>
-                    <td  onclick="setval_human('0', '1', '2', '1.0')"> <input type="hidden" id="2" value="0.5"> </td>
-                    <td  onclick="setval_human('0', '2', '3', '1.0')"> <input type="hidden" id="3" value="0.5"> </td>
-                </tr>
-                <tr>
-                    <td  onclick="setval_human('1', '0', '4','1.0')">  <input type="hidden" id="4" value="0.5"> </td>
-                    <td  onclick="setval_human('1', '1', '5', '1.0')">  <input type="hidden" id="5" value="0.5"> </td>
-                    <td  onclick="setval_human('1', '2', '6', '1.0')">  <input type="hidden" id="6" value="0.5"> </td>
-                </tr>
-                <tr>
-                    <td  onclick="setval_human('2', '0', '7', '1.0')"> <input type="hidden" id="7" value="0.5"> </td>
-                    <td  onclick="setval_human('2', '1', '8', '1.0')"> <input type="hidden" id="8" value="0.5"> </td>
-                    <td  onclick="setval_human('2', '2', '9', '1.0')"> <input type="hidden" id="9" value="0.5"> </td>
-                </tr>
-            </table>
+          <table id='board'>
+            <tr>
+              <td before="test()" onclick="setval_human('0', '0', '1', '1.0')"> <input type="hidden" id="1" value="0.5"> </td>
+              <td onclick="setval_human('0', '1', '2', '1.0')"> <input type="hidden" id="2" value="0.5"> </td>
+              <td onclick="setval_human('0', '2', '3', '1.0')"> <input type="hidden" id="3" value="0.5"> </td>
+            </tr>
+            <tr>
+              <td onclick="setval_human('1', '0', '4','1.0')"> <input type="hidden" id="4" value="0.5"> </td>
+              <td onclick="setval_human('1', '1', '5', '1.0')"> <input type="hidden" id="5" value="0.5"> </td>
+              <td onclick="setval_human('1', '2', '6', '1.0')"> <input type="hidden" id="6" value="0.5"> </td>
+            </tr>
+            <tr>
+              <td onclick="setval_human('2', '0', '7', '1.0')"> <input type="hidden" id="7" value="0.5"> </td>
+              <td onclick="setval_human('2', '1', '8', '1.0')"> <input type="hidden" id="8" value="0.5"> </td>
+              <td onclick="setval_human('2', '2', '9', '1.0')"> <input type="hidden" id="9" value="0.5"> </td>
+            </tr>
+          </table>
 
         </div>
         <div class="col-sm-6">
+          <div class="center-block">
+            <br><br><button type="button" class="btn btn-success" onclick="python()" id="send">senden</button>
+            <button onclick="clear_content()" class="btn btn-danger ">Clear</button>
 
-            <div class="center-block">
-                <br><br><button type="button" class="btn btn-success"  onclick="python()" id="send" >senden</button>
-                <button onclick="clear_content()" class="btn btn-danger " >Clear</button>
-
-                <br> <h2>Trainingsmodus </h2>
-                <input type="checkbox" class="chk">
-                <p>Im Trainingsmodus werden Trainingssätze für das Neuronale Netzwerk erzeugt. Diese Trainingssätze bilden die Grundlage für das selbständige Training und das gewichten der Neuronen. </p>
-            </div>
-
+            <br>
+            <h2>Trainingsmodus </h2>
+            <input type="checkbox" class="chk">
+            <p>Im Trainingsmodus werden Trainingssätze für das Neuronale Netzwerk erzeugt. Diese Trainingssätze bilden die Grundlage für das selbständige Training und das gewichten der Neuronen. </p>
+          </div>
         </div>
         </form>
-    </div>
+      </div>
 
-</div>
-<div class="row">
-    <div class="col-sm-6"> </div>
-    <div class="col-sm-6">
+    </div>
+    <div class="row">
+      <div class="col-sm-6"> </div>
+      <div class="col-sm-6">
         <h2>Rückgabewert</h2>
         <form class="form-horizontal">
-            <div class="form-group">
-                <label for="calc" class="col-sm-2 control-label">Berechnet</label>
-                <div class="col-sm-8">
-                    <input type="text" class="form-control" id="calc" placeholder="Berechnet">
-                </div>
+          <div class="form-group">
+            <label for="calc" class="col-sm-2 control-label">Berechnet</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="calc" placeholder="Berechnet">
             </div>
+          </div>
 
-            <h2>Trainingsätze</h2>
-            <div class="form-group">
-                <label for="input" class="col-sm-2 control-label">Input</label>
-                <div class="col-sm-8">
-                    <input type="text" class="form-control" id="input" placeholder="Input">
-                </div>
+          <h2>Trainingsätze</h2>
+          <div class="form-group">
+            <label for="input" class="col-sm-2 control-label">Input</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="input" placeholder="Input">
             </div>
+          </div>
 
-            <div class="form-group">
-                <label for="output" class="col-sm-2 control-label">Output</label>
-                <div class="col-sm-8">
-                    <input type="text" class="form-control" id="output" placeholder="Output">
-                </div>
+          <div class="form-group">
+            <label for="output" class="col-sm-2 control-label">Output</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="output" placeholder="Output">
             </div>
-            <div class="form-group">
-                <label for="zug" class="col-sm-2 control-label">Zug</label>
-                <div class="col-sm-8">
-                    <input type="text" class="form-control" id="zug" value="0" placeholder="Zug">
-                </div>
+          </div>
+          <div class="form-group">
+            <label for="zug" class="col-sm-2 control-label">Zug</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="zug" value="0" placeholder="Zug">
             </div>
-            <div class="form-group">
-                <label for="player" class="col-sm-2 control-label">Player</label>
-                <div class="col-sm-8">
-                    <input type="text" class="form-control" id="player" value="1"  placeholder="">
-                </div>
+          </div>
+          <div class="form-group">
+            <label for="player" class="col-sm-2 control-label">Player</label>
+            <div class="col-sm-8">
+              <input type="text" class="form-control" id="player" value="1" placeholder="">
             </div>
+          </div>
+      </div>
     </div>
-</div>
-</div>
+    </div>
 
-<input type="hidden" class="form-control" id="gezogen" value="0"  placeholder="">
-<!-- <button onclick="alert(ReadInput())">Zeig Input Parameter</button> -->
-</div>
+    <input type="hidden" class="form-control" id="gezogen" value="0" placeholder="">
+    <!-- <button onclick="alert(ReadInput())">Zeig Input Parameter</button> -->
+    </div>
